1566555766df3d99d51a743e3019f0b7d46155d2
vinay <vinaykr4321@gmail.com>
2020-04-26 16:55:25 +0530

    Updates

---
+++ precache-manifest.13c7cf279aac9e645110ee6f2f1b61dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e847cc6a4b756d0592b8e36f46eafd",
+    "url": "/realtime-chat/index.html"
+  },
+  {
+    "revision": "9096d626d7d2bd93c4ac",
+    "url": "/realtime-chat/static/css/main.1ce55ba0.chunk.css"
+  },
+  {
+    "revision": "a08e55919d8e2c5c691a",
+    "url": "/realtime-chat/static/js/2.a05deb67.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/realtime-chat/static/js/2.a05deb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9096d626d7d2bd93c4ac",
+    "url": "/realtime-chat/static/js/main.780458c4.chunk.js"
+  },
+  {
+    "revision": "f4520c9dfedfb09efa6f",
+    "url": "/realtime-chat/static/js/runtime-main.bc8d3c98.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/realtime-chatapp/precache-manifest.c7b25e5c6074ef707e532d31808f0f9d.js"
+  "/realtime-chat/precache-manifest.13c7cf279aac9e645110ee6f2f1b61dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/realtime-chatapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/realtime-chat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
